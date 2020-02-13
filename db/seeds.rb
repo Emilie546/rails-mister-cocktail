a786da7ece15826106ee10c69ba196eff14ab50a
Emilie Keller <emilie.keller@bluewin.ch>
2020-02-13 17:29:47 +0100

    first-day cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+buffer = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+result = JSON.parse(buffer)
+drinks_list = result['drinks']
+
+drinks_list.each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
